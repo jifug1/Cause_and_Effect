58c76f4337201bafce697880bf3e7e5b6f3187fb
jifug1 <kaktus001b@gmail.com>
2026-06-15 05:44:12 +0300

    4.2.7

---
+++ text.h
@@ -0,0 +1,18 @@
+#ifndef TEXT_H
+#define TEXT_H
+std::string txt1 = " МЕНЮ\n нажмите 1 (затем ENTER) чтобы начать играть. нажмите 2 (затем ENTER) чтобы продолжить игру\n\n\n";
+std::string txt2 = "";
+std::string txt3 = "";
+std::string txt4 = "";
+std::string txt5 = "";
+std::string txt6 = "";
+std::string txt7 = "";
+std::string txt8 = "";
+std::string txt9 = "";
+std::string txt10 = "";
+std::string txt11 = "";
+std::string txt12 = "";
+std::string txt13 = "";
+std::string txt14 = "";
+std::string txt15 = "";
+#endif
